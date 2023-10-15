3d50c73a0a8ff3475a22b3e400831dea80ce9285
ahmetkosker <exarons@gmail.com>
2023-10-15 20:28:04 +0300

    ss

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Aside from "../components/Home/Aside";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
-import axios from "axios";
+import axios, { all } from "axios";
 import { Pagination } from "@mui/material";
 import LoadingComponent from "../components/LoadingComponent";
 
@@ -13,27 +13,13 @@ const Blogs = () => {
 
   const [allBlogs, setAllBlogs] = useState(null);
 
-  const [page, setPage] = useState(1);
-
-  const [blogs, setBlogs] = useState([]);
-
-  const handleChange = (event, value) => {
-    setPage(value);
-  };
-
   useEffect(() => {
     axios
       .get("https://getallblogposts-zkwsxnxtga-ew.a.run.app")
       .then((res) => setAllBlogs(res.data));
   }, []);
 
-  useEffect(() => {
-    if (allBlogs) {
-      const start = (page - 1) * 3;
-      const end = start + 3;
-      setBlogs(allBlogs.slice(start, end));
-    }
-  }, [page, allBlogs]);
+  console.log(allBlogs)
 
   if (allBlogs === null)
     return (
@@ -50,12 +36,12 @@ const Blogs = () => {
           alt="BG"
         />
         <div className="flex flex-col items-center relative right-10 gap-y-5 xl:gap-y-7 2xl:gap-y-12  ml-3">
-          {blogs.map((blog, index) => {
+          {allBlogs.map((blog, index) => {
             return (
               <div
                 className={`${test === index
-                    ? "scale-110 sm:h-80 h-54 sm:w-3/5 w-4/5 lg:h-96"
-                    : "sm:h-32 h-20 sm:w-3/5 w-4/5"
+                  ? "scale-110 sm:h-80 h-52 sm:w-3/5 w-4/5 lg:h-96"
+                  : "sm:h-32 h-20 sm:w-3/5 w-full"
                   }  relative origin-bottom-right transition-all ease-in overflow-hidden bg-[#D9D9D9] opacity-80 py-2 px-2 sm:py-5 sm:px-7 rounded-2xl mt-2 flex flex-col ml-14 sm:ml-0`}
               >
                 <div
@@ -66,14 +52,14 @@ const Blogs = () => {
                     src={blog.docData.blogPostImageURL}
                     alt="banner"
                     className={`rounded-2xl ${test === index
-                        ? "w-12 h-12 lg:w-16 lg:h-16 lg:mt-2 lg:ml-2 mt-3 ml-2 sm:w-24 sm:h-24 scale-125 sm:mt-6 sm:ml-5"
-                        : "w-12 h-12 lg:w-16 lg:h-16 lg:mt-2 lg:ml-2 sm:w-24 sm:h-24"
+                      ? "w-12 h-12 lg:w-16 lg:h-16 lg:mt-2 lg:ml-2 mt-3 ml-2 sm:w-24 sm:h-24 scale-125 sm:mt-6 sm:ml-5"
+                      : "w-12 h-12 lg:w-16 lg:h-16 lg:mt-2 lg:ml-2 sm:w-24 sm:h-24"
                       } transition-all origin-bottom-right object-cover`}
                   />
                   <p
                     className={`font-bold sm:text-lg sm:ml-5 transition-all text-xs ml-2 ${test === index
-                        ? "opacity-100 sm:mt-1 lg:text-xs"
-                        : "opacity-70 sm:mt-2 lg:text-xs"
+                      ? "opacity-100 sm:mt-1 lg:text-xs"
+                      : "opacity-70 sm:mt-2 lg:text-xs"
                       }`}
                   >
                     {blog.docData.blogPostTitle}
@@ -82,8 +68,8 @@ const Blogs = () => {
                 <p
                   onClick={() => setTest(index)}
                   className={`font-bold sm:text-sm w-full sm:mt-2 mt-3 ml-0 sm:ml-5 self-end absolute transition-all text-[7px] ${test === index
-                      ? "sm:top-20 lg:text-xs sm:left-36 top-8 left-[72px] lg:left-[100px] lg:top-6"
-                      : "sm:top-3/4 lg:text-xs sm:left-2/3 top-12 left-32"
+                    ? "sm:top-20 lg:text-xs sm:left-36 top-8 left-[72px] lg:left-[100px] lg:top-6"
+                    : "sm:top-3/4 lg:text-xs sm:left-2/3 top-12 left-36"
                     }`}
                 >
                   {blog.docData.blogPostAuthor}
@@ -96,20 +82,12 @@ const Blogs = () => {
                     alt="Close"
                   />
                 )}
-                <div className="mt-8 font-bold sm:text-sm text-[6px] lg:text-[9px] 2xl:text-sm">
+                <div className="mt-8 font-bold sm:text-sm text-[6px] lg:text-[9px] 2xl:text-sm overflow-y-scroll">
                   {blog.docData.blogPostContent}
                 </div>
               </div>
             );
           })}
-          <div className="ml-14 sm:ml-0">
-            <Pagination
-              page={page}
-              onChange={handleChange}
-              count={Math.ceil(allBlogs.length / 3)}
-              color="secondary"
-            />
-          </div>
         </div>
 
         <div>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -40,7 +40,7 @@ const Projects = () => {
                   />
                 </div>
 
-                <div className="w-36 xl:w-60 xl:mt-5 mt-2 group-hover:mt-0 xl:pt-9 pt-2 group-hover:pt-9 xl:group-hover:pt-10 duration-500 text-[8px] xl:text-lg group-hover:h-40 xl:group-hover:h-60 overflow-hidden h-16 xl:h-32 bg-[#A59719] transition-opacity rounded-b-3xl text-white text-left px-3">
+                <div className="w-36 xl:w-60 xl:mt-5 mt-2 group-hover:mt-0 xl:pt-9 pt-2 group-hover:pt-9 xl:group-hover:pt-10 duration-500 text-[8px] xl:text-lg group-hover:h-auto pb-3 xl:group-hover:h-60 overflow-hidden h-16 xl:h-32 bg-[#A59719] transition-opacity rounded-b-3xl text-white text-left px-3">
                   <div className="group-hover:translate-x-3 xl:group-hover:translate-x-4 group-hover:-translate-y-7 w-48 transition-all duration-500">
                     {project.docData.artistName} -{" "}
                     {project.docData.artistJobTitle}
